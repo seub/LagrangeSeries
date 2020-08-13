66f2e47629cd5fcb4199bc22c068192aad9f7ebc
seub <brice.loustau@gmail.com>
2020-08-13 16:16:21 +0200

    Added generated Markov lists

---
+++ markov-generate.py
@@ -0,0 +1,42 @@
+from numpy import *
+import matplotlib.pyplot as plt
+
+		
+def vietaTop(triple) :
+	return [triple[0], triple[2], 3*triple[0]*triple[2]-triple[1]]
+	
+def vietaBottom(triple) :
+	return [triple[1],triple[2],3*triple[1]*triple[2]-triple[0]]
+
+def generateMarkovRec(triple, prevList, upperBound) :
+	
+	if triple[2] < upperBound :
+		prevList.append(triple[2])
+		generateMarkovRec(vietaTop(triple), prevList, upperBound)
+		generateMarkovRec(vietaBottom(triple), prevList, upperBound)
+	else:
+		return triple
+		
+def generateMarkov(upperBound):
+	res = []
+	generateMarkovRec([1,1,1], res, upperBound)
+	return sorted(set(res))
+	
+def generateMarkov(upperBound):
+	res = []
+	generateMarkovRec([1,1,1], res, upperBound)
+	return sorted(set(res))
+	
+def printMarkovList(upperBound):
+	markovList = generateMarkov(upperBound)
+	L = len(markovList)
+	print(L)
+	filename = 'Markov'+str(L)+'.txt'
+	with open(filename, mode='wt', encoding='utf-8') as f:
+    		f.write('\n'.join(str(m) for m in markovList))
+
+	
+upperBound = 10**400
+printMarkovList(upperBound)
+
+
